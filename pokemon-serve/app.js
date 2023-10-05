94160d09f200a907b253ae03f96a540922f8ccfd
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 16:59:53 +0900

    메인 / 피카츄 ~ 꼬부기 페이지 이동이 원활한지 확인. 이전/다음 페이지가 없는 경우를 조건식으로 작성하려 했으나 실패해서 주석 처리

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -10,8 +10,17 @@ const http = require("http");
 
 const server = http.createServer(function (request, response) {
   console.log(request.url);
-
-  // 피카츄
+  // index 0번 메인 페이지
+  if (request.url === "/0") {
+    const mainPage = `<html>
+    <head><title>Main</title></head>
+    <body>
+      <h1>Main</h1>
+    </body>
+    </html>`;
+    response.end(mainPage);
+  }
+  // index 1번 피카츄 페이지
   if (request.url === "/1") {
     const pagePikachu = `<html>
     <head><title>Pikachu</title></head>
@@ -20,10 +29,10 @@ const server = http.createServer(function (request, response) {
       <a href="http://localhost:1234/2">Next</a>
       <h1>Pikachu</h1>
     </body>
-  </html>`;
+    </html>`;
     response.end(pagePikachu);
   }
-  // 라이츄
+  // index 2번 라이츄 페이지
   if (request.url === "/2") {
     const pageRaichu = `<html>
     <head><title>Raichu</title></head>
@@ -32,9 +41,46 @@ const server = http.createServer(function (request, response) {
       <a href="http://localhost:1234/3">Next</a> 
       <h1>Raichu</h1>
     </body>
-  </html>`;
+    </html>`;
     response.end(pageRaichu);
   }
+  // index 3번 파이리 페이지
+  if (request.url === "/3") {
+    const pageCharmander = `<html>
+    <head><title>Charmander</title></head>
+    <body>
+      <a href="http://localhost:1234/2">Previous</a> 
+      <a href="http://localhost:1234/4">Next</a> 
+      <h1>Charmander</h1>
+    </body>
+    </html>`;
+    response.end(pageCharmander);
+  }
+  // index 4번 꼬부기 페이지
+  if (request.url === "/4") {
+    const pageSquirtle = `<html>
+    <head><title>Squirtle</title></head>
+    <body>
+      <a href="http://localhost:1234/3">Previous</a> 
+      <a href="http://localhost:1234/5">Next</a> 
+      <h1>Squirtle</h1>
+    </body>
+    </html>`;
+    response.end(pageSquirtle);
+  }
+  // 이전/다음 페이지가 없는 경우를 조건식으로 작성 -> 메인 페이지로 이동
+  //! -> 실패해서 주석처리
+  // if (response.writeHead(404)) {
+  //   const response404 = `<html>
+  // <head></head>
+  // <body>
+  //   <script>
+  //   window.location.href = "http://localhost:1234/0";
+  //   </script>
+  // </body>
+  // </html>`;
+  //   response.end(response404);
+  // }
 
   response.writeHead(200, { "Content-Type": "text/html" });
 });
