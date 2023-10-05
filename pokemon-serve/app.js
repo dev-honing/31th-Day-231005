6667e83d49b3f172c7b2ba9b1138e7109c66f844
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 14:41:49 +0900

    조건문을 통해 응답을 2가지로 만들어 멀티 페이지 구성

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -3,15 +3,6 @@ const http = require("http");
 const server = http.createServer(function (request, response) {
   console.log(request.url);
 
-  // 조건문 작성
-  if (request.url === "/") {
-    console.log("메인입니다."); // 주소창에 localhost:1234를 입력해 접속하면 "메인입니다."가 출력
-  }
-  if (request.url === "/sub") {
-    console.log("서브입니다."); // 주소창에 localhost:1234/sub를 입력해 접속하면 "서브입니다."가 출력
-  }
-
-  response.writeHead(200, { "Content-Type": "text/html" });
   // html의 구성을 doc로 변수 선언
   let doc = `<html>
       <head></head>
@@ -20,7 +11,29 @@ const server = http.createServer(function (request, response) {
       </body>
     </html>`;
 
-  response.end(doc);
+  // 조건문 작성을 통한 응답을 2가지로 만들어 멀티 페이지 구성
+  // 메인페이지
+  if (request.url === "/") {
+    response.end(`<html>
+      <head></head>
+      <body>
+        <h1>Main Page</h1>
+      </body>
+    </html>`);
+  }
+  // 서브페이지
+  if (request.url === "/sub") {
+    response.end(`<html>
+      <head></head>
+      <body>
+        <h1>Sub Page</h1>
+      </body>
+    </html>`);
+  }
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+
+  // response.end(doc);
 });
 
 server.listen(1234);
