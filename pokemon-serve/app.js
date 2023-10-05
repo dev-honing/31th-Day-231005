830e219c3f69837553fb01debfa3a4f8d596cdc4
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 15:50:20 +0900

    1을 피카츄, 2를 라이츄로 변경하고 Previous / Next를 <a> 태그로 만들어 이동할 수 있게 함

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -4,22 +4,24 @@ const server = http.createServer(function (request, response) {
   console.log(request.url);
 
   // 피카츄
-  if (request.url === "/pikachu") {
+  if (request.url === "/1") {
     const pagePikachu = `<html>
     <head><title>Pikachu</title></head>
     <body>
-      <a href="http://localhost:1234/sub">Go to SubPage</a>
+      <a href="http://localhost:1234/0">Previous</a>
+      <a href="http://localhost:1234/2">Next</a>
       <h1>Pikachu</h1>
     </body>
   </html>`;
     response.end(pagePikachu);
   }
   // 라이츄
-  if (request.url === "/raichu") {
+  if (request.url === "/2") {
     const pageRaichu = `<html>
     <head><title>Raichu</title></head>
     <body>
-      <a href="http://localhost:1234/">Go to MainPage</a> 
+      <a href="http://localhost:1234/1">Previous</a> 
+      <a href="http://localhost:1234/3">Next</a> 
       <h1>Raichu</h1>
     </body>
   </html>`;
