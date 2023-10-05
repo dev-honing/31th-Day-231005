92b8280f939b6d09508e949c2cca1dc43ff53ada
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 10:51:05 +0900

    데이터 타입을 검사해보면 object임을 확인

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 
 const server = http.createServer(function (request, response) {
-  console.log(response);
+  console.log(typeof response);
 });
 
 server.listen(1234);
