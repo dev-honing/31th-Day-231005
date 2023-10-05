c6344726733a06b5c2e50a3925f07b2213af99f6
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 15:17:23 +0900

    <title> 태그와 제목 작성 후, <h1> 태그 편집

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -15,9 +15,10 @@ const server = http.createServer(function (request, response) {
   // 메인페이지
   if (request.url === "/") {
     const mainPage = `<html>
-    <head></head>
+    <head><title>Main Page</title></head>
     <body>
-      <h1>Main Page</h1>
+      <a href="http://localhost:1234/sub">Go to SubPage</a>
+      <h1>Welcome! This is Main Page</h1>
     </body>
   </html>`;
     response.end(mainPage);
@@ -25,9 +26,10 @@ const server = http.createServer(function (request, response) {
   // 서브페이지
   if (request.url === "/sub") {
     const subPage = `<html>
-    <head></head>
+    <head><title>Sub Page</title></head>
     <body>
-      <h1>Sub Page</h1>
+      <a href="http://localhost:1234/">Go to MainPage</a> 
+      <h1>Welcome! This is Sub Page</h1>
     </body>
   </html>`;
     response.end(subPage);
