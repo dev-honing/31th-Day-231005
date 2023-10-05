27972746c314a20ef3e8e637f7aa96bc4ffd32d1
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 14:28:22 +0900

    조건문 작성하고 메인입니다.가 출력되는지 확인

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -3,6 +3,14 @@ const http = require("http");
 const server = http.createServer(function (request, response) {
   console.log(request.url);
 
+  // 조건문 작성
+  if (request.url === "/") {
+    console.log("메인입니다.");
+  }
+  if (request.url === "/sub") {
+    console.log("서브입니다.");
+  }
+
   response.writeHead(200, { "Content-Type": "text/html" });
   // html의 구성을 doc로 변수 선언
   let doc = `<html>
