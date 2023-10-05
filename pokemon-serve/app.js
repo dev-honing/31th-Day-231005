34c34589cd36d63fe1298f3dff6c1feb576c2133
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 10:54:41 +0900

    writeHead() 메서드 사용하고 프로토콜에 맞게 응답사항 작성

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -2,6 +2,9 @@ const http = require("http");
 
 const server = http.createServer(function (request, response) {
   console.log(typeof response);
+
+  response.writeHead(200, { "Content-Type": "text/html" });
+  response.end("안녕!");
 });
 
 server.listen(1234);
