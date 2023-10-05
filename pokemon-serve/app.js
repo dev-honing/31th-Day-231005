0850cb40c4fa90b153142fed748a03fe660f7966
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-05 15:46:37 +0900

    메인 페이지는 피카츄, 서브 페이지는 라이츄로 변경

--- pokemon-serve/app.js
+++ pokemon-serve/app.js
@@ -3,36 +3,27 @@ const http = require("http");
 const server = http.createServer(function (request, response) {
   console.log(request.url);
 
-  // html의 구성을 doc로 변수 선언
-  let doc = `<html>
-      <head></head>
-      <body>
-        <h1>hello!</h1>
-      </body>
-    </html>`;
-
-  // 조건문 작성을 통한 응답을 2가지로 만들어 멀티 페이지 구성
-  // 메인페이지
-  if (request.url === "/") {
-    const mainPage = `<html>
-    <head><title>Main Page</title></head>
+  // 피카츄
+  if (request.url === "/pikachu") {
+    const pagePikachu = `<html>
+    <head><title>Pikachu</title></head>
     <body>
       <a href="http://localhost:1234/sub">Go to SubPage</a>
-      <h1>Welcome! This is Main Page</h1>
+      <h1>Pikachu</h1>
     </body>
   </html>`;
-    response.end(mainPage);
+    response.end(pagePikachu);
   }
-  // 서브페이지
-  if (request.url === "/sub") {
-    const subPage = `<html>
-    <head><title>Sub Page</title></head>
+  // 라이츄
+  if (request.url === "/raichu") {
+    const pageRaichu = `<html>
+    <head><title>Raichu</title></head>
     <body>
       <a href="http://localhost:1234/">Go to MainPage</a> 
-      <h1>Welcome! This is Sub Page</h1>
+      <h1>Raichu</h1>
     </body>
   </html>`;
-    response.end(subPage);
+    response.end(pageRaichu);
   }
 
   response.writeHead(200, { "Content-Type": "text/html" });
